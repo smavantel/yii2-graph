2e4ae4447f29cc3fac7ed5516383e52ca4170012
Stefan Mager <s.mager@avantel.de>
2023-01-19 12:49:26 +0100

    composer

--- src/GraphJsonConverter.php
+++ src/GraphJsonConverter.php
@@ -1,11 +1,5 @@
 <?php
-
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/PHPClass.php to edit this template
- */
-
-namespace common\modules\mail\components\graph;
+namespace smavantel\graph;
 
 /**
  * Description of GraphEmailAddress
